8caa6e140d293347497d8e2a1c37f72f2fcda54c
Jin-Sih Lin <linpct@gmail.com>
2016-04-03 14:51:11 +0800

    sync install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ cd ~
 git clone git://github.com/pct/vimrc-core.git
 mv .vimrc .vimrc.yours
 mv .vim .vim.yours
-cd vimrc-core; ./update.sh; cd -
 ln -s vimrc-core/.vimrc
 ln -s vimrc-core/.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd vimrc-core; ./update.sh; cd -
