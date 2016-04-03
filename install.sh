6ea07cb0631b4bc69a5222fbaaa7ebd7f88d49f9
Jin-Sih Lin <linpct@gmail.com>
2016-04-03 06:55:12 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,4 @@ mv .vimrc .vimrc.yours
 mv .vim .vim.yours
 ln -s vimrc-core/.vimrc
 ln -s vimrc-core/.vim
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 cd vimrc-core; ./update.sh; cd -
